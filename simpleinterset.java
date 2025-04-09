7e501c68ce35b4f51fec0f19e0f1b6a46586bae8
Ranjitrevanakar02 <Ranjitrevanakar02@gmail.com>
2025-04-09 15:56:51 +0530

    simpleinterset.java

---
+++ simpleinterset.java
@@ -0,0 +1,11 @@
+public class simpleinterset {
+   public static void main(String[] args) {
+    int P=1000;
+    int R=10;
+    int T=2;
+
+    float SI=(P*T*R)/100;
+
+    System.err.println("The simple Intreset of PTR is"+SI);
+   }
+}
